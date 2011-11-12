c3ddc97e9c961f6142a01f0183cdd9d1a106499d
Kazunori Jo (itochan) <itochan315@gmail.com>
2011-11-13 04:28:58 +0900

    add home of screen

--- Library/Formula/screen.rb
+++ Library/Formula/screen.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Screen < Formula
   url 'ftp://ftp.gnu.org/gnu/screen/screen-4.0.3.tar.gz'
-  homepage ''
+  homepage 'http://www.gnu.org/software/screen/'
   md5 '8506fd205028a96c741e4037de6e3c42'
 
   # depends_on 'cmake'
